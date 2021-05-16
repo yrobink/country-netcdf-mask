323d34dcfc19d76edaf165930e0d133d2f7b17d6
Yoann Robin <yoann.robin.k@gmail.com>
2021-05-16 08:23:54 +0200

    Add Italy

--- run.sh
+++ run.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-#python3 exec.py -i examples/params_WGS84.txt -o output/mask_France_WGS84.nc -src gadm36,FRA,0
+## France, with lat/lon coordinates
+#python3 exec.py -i examples/params_WGS84_FRA.txt -o output/mask_France_WGS84.nc -src gadm36,FRA,0
 
-#python3 exec.py -i examples/params_WGS84.txt -o output/mask_France_Occitanie_WGS84.nc -src gadm36,FRA,1,NAME_1=Occitanie -list -bounds
-#python3 exec.py -i examples/params_WGS84.txt -o output/mask_France_Occitanie_WGS84.nc -src gadm36,FRA,1,NAME_1=Occitanie
+## Print list of regions, and bounds of Occitanie
+#python3 exec.py -i examples/params_WGS84_FRA.txt -o output/mask_France_Occitanie_WGS84.nc -src gadm36,FRA,1,NAME_1=Occitanie -list -bounds
 
-python3 exec.py -i examples/params_LambertIIcarto.txt -o output/mask_France_LambertII.nc -src gadm36,FRA,0 -bounds
-#python3 exec.py -i examples/params_LambertIIcarto.txt -o output/mask_France_LambertII.nc -src gadm36,FRA,0
+## Occitanie, in France, with lat/lon coordinates
+#python3 exec.py -i examples/params_WGS84_FRA.txt -o output/mask_France_Occitanie_WGS84.nc -src gadm36,FRA,1,NAME_1=Occitanie
 
-#python3 exec.py -i examples/params_WGS84.txt -o output/mask_France_Occitanie_WGS84.nc -src gadm36,DNK,1,NAME_1=Occitanie --not-verbose -list
+## France, in Lambert II carto, just print bounds
+#python3 exec.py -i examples/params_LambertIIcarto_FRA.txt -o output/mask_France_LambertII.nc -src gadm36,FRA,0 -bounds
+
+## France, in Lambert II carto
+#python3 exec.py -i examples/params_LambertIIcarto_FRA.txt -o output/mask_France_LambertII.nc -src gadm36,FRA,0
+
+## Italy, and Sicily in WGS84 coordinates
+#python3 exec.py -i examples/params_WGS84_ITA.txt -o output/mask_Italy_WGS84.nc -src gadm36,ITA,0 -bounds
+#python3 exec.py -i examples/params_WGS84_ITA.txt -o output/mask_Italy_WGS84.nc -src gadm36,ITA,0
+#python3 exec.py -i examples/params_WGS84_ITA.txt -o output/mask_Italy_Sicily_WGS84.nc -src gadm36,ITA,1,NAME_1=Sicily -list
+#python3 exec.py -i examples/params_WGS84_ITA.txt -o output/mask_Italy_Sicily_WGS84.nc -src gadm36,ITA,1,NAME_1=Sicily
 
 
 
